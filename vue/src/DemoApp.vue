ff6ab31c17bc1d2388c98e138c813275e1a1b649
shyam prasad <shyam>
2021-05-28 01:20:42 +0530

    first

--- vue/src/DemoApp.vue
+++ vue/src/DemoApp.vue
@@ -2,29 +2,36 @@
 import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
+
 import { INITIAL_EVENTS, createEventId } from './event-utils'
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import  { Draggable } from '@fullcalendar/interaction';
+import swal from 'sweetalert';
 
 export default {
-
+  
   components: {
     FullCalendar // make the <FullCalendar> tag available
   },
-
+   
   data: function() {
+    
     return {
       calendarOptions: {
         plugins: [
           dayGridPlugin,
           timeGridPlugin,
-          interactionPlugin // needed for dateClick
+          interactionPlugin,
+          resourceTimeGridPlugin // needed for dateClick
         ],
         headerToolbar: {
           left: 'prev,next today',
           center: 'title',
           right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
-        initialView: 'dayGridMonth',
+        initialView: 'resourceTimeGridDay',
+        resources: [{"id":"a","title":"1"},{"id":"b","title":"2"},{"id":"c","title":"3"},{"id":"d","title":"4"}],
         initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
         editable: true,
         selectable: true,
@@ -33,23 +40,72 @@ export default {
         weekends: true,
         select: this.handleDateSelect,
         eventClick: this.handleEventClick,
-        eventsSet: this.handleEvents
+        eventsSet: this.handleEvents,
+        droppable:true,
+        
+        drop: function(dropInfo) {
+      //alert(dropInfo.draggedEl.data + " is dropped at " + dropInfo.date);
+      //console.log(dropInfo.draggedEl.innerText + " is dropped at " + dropInfo.date);
+      
+     console.log(INITIAL_EVENTS);
+     if(dropInfo.resource.id==INITIAL_EVENTS[0].resourceId){
+          swal( dropInfo.draggedEl.innerText + " is dropped at " + INITIAL_EVENTS[0].title );
+     }
+     else if(dropInfo.resource.id==INITIAL_EVENTS[1].resourceId) {
+        swal( dropInfo.draggedEl.innerText + " is dropped at " + INITIAL_EVENTS[1].title );
+     }
+     
+     
+    }
+    
         /* you can update a remote database when these fire:
         eventAdd:
         eventChange:
         eventRemove:
         */
+       
       },
       currentEvents: []
     }
   },
-
+    mounted(){
+      
+       let draggableEl = document.getElementById('external-events-list');
+            new Draggable(draggableEl, {
+      itemSelector: '.fc-event',
+      eventData: function(eventEl) {
+        return {
+          //title: eventEl.innerText.trim()
+        };
+      }
+    });
+     let draggableE2 = document.getElementById('external-events-list-2');
+            new Draggable(draggableE2, {
+      itemSelector: '.fc-event',
+      eventData: function(eventEl) {
+        return {
+          //title: eventEl.innerText.trim()
+        };
+      }
+    });
+    },
   methods: {
 
     handleWeekendsToggle() {
       this.calendarOptions.weekends = !this.calendarOptions.weekends // update a property
     },
-
+      dropremove(arg){
+        
+        // is the "remove after drop" checkbox checked?
+        // if (document.getElementById('drop-remove').checked) {
+        //   // if so, remove the element from the "Draggable Events" list
+        //   arg.draggedEl.parentNode.removeChild(arg.draggedEl);
+        // }
+        //console.log(arg)
+        console.log(arg.draggedEl.childNodes[1].childNodes[0].data + " is dropped at " + arg.date);
+        //console.log(arg.draggedEl.childNodes[1].value);
+      },
+      
     handleDateSelect(selectInfo) {
       let title = prompt('Please enter a new title for your event')
       let calendarApi = selectInfo.view.calendar
@@ -72,17 +128,19 @@ export default {
         clickInfo.event.remove()
       }
     },
-
+     
     handleEvents(events) {
       this.currentEvents = events
-    }
+    },
+    
   }
 }
 </script>
 
 <template>
+
   <div class='demo-app'>
-    <div class='demo-app-sidebar'>
+    <div class='demo-app-sidebar' style="display:none">
       <div class='demo-app-sidebar-section'>
         <h2>Instructions</h2>
         <ul>
@@ -111,7 +169,64 @@ export default {
         </ul>
       </div>
     </div>
+
     <div class='demo-app-main'>
+      <div class="wrap-right">
+    <div id='external-events'>
+      <h4>Drivers list</h4>
+
+      <div id='external-events-list'>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="dr1">Driver 1</div>
+        </div>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="dr2">Driver 2</div>
+        </div>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="dr3">Driver 3</div>
+        </div>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="dr4">Driver 4</div>
+        </div>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="dr5">Driver 5</div>
+        </div>
+      </div>
+
+      <p>
+        <input type='checkbox' id='drop-remove' />
+        <label for='drop-remove'>remove after drop</label>
+      </p>
+    </div>
+
+
+    <div id='external-events-2'>
+      <h4>Tractors list</h4>
+
+      <div id='external-events-list-2'>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="tr1">Tractors event 1</div>
+        </div>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="tr2">Tractors event 2</div>
+        </div>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="tr3">Tractors event 3</div>
+        </div>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="tr4">Tractors event 4</div>
+        </div>
+        <div class='fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event'>
+          <div class='fc-event-main' id="tr5">Tractors event 5</div>
+        </div>
+      </div>
+
+      <p>
+        <input type='checkbox' id='drop-remove' />
+        <label for='drop-remove'>remove after drop</label>
+      </p>
+    </div>
+  </div>
       <FullCalendar
         class='demo-app-calendar'
         :options='calendarOptions'
@@ -121,12 +236,81 @@ export default {
           <i>{{ arg.event.title }}</i>
         </template>
       </FullCalendar>
+      
     </div>
   </div>
 </template>
 
 <style lang='css'>
+#external-events {
+   position: relative;
+    
+    right: 40px;
+    top: 20px;
+    width: 200px;
+    padding: 0 10px;
+    border: 1px solid #ccc;
+    background: #eee;
+    text-align: left;
+  }
+  #external-events-2 {
+   position: relative;
+    
+    right: 40px;
+    top: 80px;
+    width: 200px;
+    padding: 0 10px;
+    border: 1px solid #ccc;
+    background: #eee;
+    text-align: left;
+  }
+  #external-events h4 {
+    font-size: 16px;
+    margin-top: 0;
+    padding-top: 1em;
+  }
 
+  #external-events .fc-event {
+    margin: 3px 0;
+    cursor: move;
+  }
+
+  #external-events p {
+    margin: 1.5em 0;
+    font-size: 11px;
+    color: #666;
+  }
+
+  #external-events p input {
+    margin: 0;
+    vertical-align: middle;
+  }
+  #external-events-2 h4 {
+    font-size: 16px;
+    margin-top: 0;
+    padding-top: 1em;
+  }
+
+  #external-events-2 .fc-event {
+    margin: 3px 0;
+    cursor: move;
+  }
+
+  #external-events-2 p {
+    margin: 1.5em 0;
+    font-size: 11px;
+    color: #666;
+  }
+
+  #external-events-2 p input {
+    margin: 0;
+    vertical-align: middle;
+  }
+
+  
+.wrap-right{
+  float: right;
+}
 h2 {
   margin: 0;
   font-size: 16px;
